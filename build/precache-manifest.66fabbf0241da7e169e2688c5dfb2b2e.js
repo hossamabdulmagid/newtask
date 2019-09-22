a2b150a737d0bf6f41103b4eaeda1e39722e19e8
hossamabdulmagid <hosamabdulmaged@gmail.com>
2019-09-22 05:31:58 +0200

    Adding Hosting(https://sawsaw-project.web.app/)

---
+++ build/precache-manifest.66fabbf0241da7e169e2688c5dfb2b2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fc811cc2551c1b235498bd0afecd87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b29a97f7a333edfdebd2",
+    "url": "/static/css/main.496c13fa.chunk.css"
+  },
+  {
+    "revision": "2e49bc3e19612f83d393",
+    "url": "/static/js/2.1b4414b5.chunk.js"
+  },
+  {
+    "revision": "b29a97f7a333edfdebd2",
+    "url": "/static/js/main.013c91d0.chunk.js"
+  },
+  {
+    "revision": "6838976dbb7bb89ff216",
+    "url": "/static/js/runtime~main.7fac486a.js"
+  }
+]);
